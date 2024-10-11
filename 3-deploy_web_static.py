fc32c4c0b099c77db1cca9bb9e4535f5e22f8b6e
L.Semakale <155956058+L-Semakale@users.noreply.github.com>
2024-10-11 22:59:05 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,35 +1,27 @@
-#!/usr/bin/python3
-"""
-Fabric script that creates and distributes an archive to the web servers.
-
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
-"""
-
+# tasks.py
 from fabric import task
-from fabric.connection import Connection
 from datetime import datetime
 from os.path import exists, isdir
-from invoke import UnexpectedExit
+from invoke import run as local
 
 env.hosts = ['54.159.37.237', '54.90.3.106']
 
 @task
 def do_pack(c):
-    """Generates a tgz archive."""
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            c.run("mkdir versions")
+            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-        c.run("tar -cvzf {} web_static".format(file_name))
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
-        print(f"Error creating archive: {e}")
+    except:
         return None
 
 @task
 def do_deploy(c, archive_path):
-    """Distributes an archive to the web servers."""
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
@@ -45,15 +37,14 @@ def do_deploy(c, archive_path):
         c.run('rm -rf /data/web_static/current')
         c.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except UnexpectedExit as e:
-        print(f"Deployment failed: {e}")
+    except:
         return False
 
 @task
 def deploy(c):
-    """Creates and distributes an archive to the web servers."""
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack(c)
     if archive_path is None:
         return False
-    result = do_deploy(c, archive_path)
-    return result
+    return do_deploy(c, archive_path)
+
