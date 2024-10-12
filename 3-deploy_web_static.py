f0fccc79b8e2fef6a96ce4aad818b03c791db307
L.Semakale <155956058+L-Semakale@users.noreply.github.com>
2024-10-12 10:45:12 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,7 +12,7 @@ from datetime import datetime
 from os.path import exists, isdir
 import os
 
-hosts = ['54.159.37.237', '54.90.3.106']
+hosts = ['98.84.112.27', '54.234.223.92']
 
 @task
 def do_pack():
